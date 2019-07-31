38a926190d15d03e0ca28ec1d1659bb2e0281e3b
Clinton Ingram <clinton.ingram@outlook.com>
2019-07-31 14:02:02 -0700

    refactor for readability, remove big-endian code (can't test it anyway)

---
+++ src/Blake2Fast/ThrowHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SauceControl.Blake2Fast
+{
+	internal static class ThrowHelper
+	{
+		public static void HashFinalized() => throw new InvalidOperationException("Hash has already been finalized.");
+
+		public static void NoBigEndian() => throw new PlatformNotSupportedException("Big-endian platforms not supported");
+
+		public static void DigestInvalidLength(int max) => throw new ArgumentOutOfRangeException("digestLength", $"Value must be between 1 and {max}");
+
+		public static void KeyTooLong(int max) => throw new ArgumentException($"Key must be between 0 and {max} bytes in length", "key");
+	}
+}
